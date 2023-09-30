7b1980c09086bd8309040b916ea3bc12799f646b
Santan21 <lesliegyamfi02@gmail.com>
2023-09-30 16:48:48 +0000

    commit 1

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Data Source, could be replaced with a real database
+const todos = [
+    {
+      title: "Todo 1",
+      desc: "This is my first Todo",
+      completed: true,
+    },
+  
+    {
+      title: "Todo 2",
+      desc: "This is my second Todo",
+      completed: true,
+    },
+  
+    {
+      title: "Todo 3",
+      desc: "This is my third Todo",
+      completed: true,
+    },
+  
+    {
+      title: "Todo 4",
+      desc: "This is my fourth Todo",
+      completed: true,
+    },
+  
+    {
+      title: "Todo 5",
+      desc: "This is my fifth Todo",
+      completed: true,
+    },
+  ];
+  // Data source ends here
